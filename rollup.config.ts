8ad425754288883cf38ab9ae4d50357355cfdfad
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-15 09:47:00 +0200

    Update rollup.config.ts
     I added the following:
    
    isProduction constant: This is a boolean flag that is set based on the NODE_ENV environment variable. It is used to determine whether or not to minify the JavaScript in production.
    target and lib options to the typescript plugin: These options allow you to specify the ECMAScript version that the compiled JavaScript code should target and the list of built-in libraries that the compiler should include.
    watch option: This allows you to watch for changes to the source files and automatically rebuild when changes are detected.
    cache option: This allows you to customize the Rollup cache, which can help speed up the build process by avoiding unnecessary recompilations.
    
    I also made some minor changes to the existing code:
    
    I changed the import statement for terser to use a destructuring import instead of importing the default export.
    I added a type annotation for the rollupConfig variable to make it clear what type of object it represents.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,24 +1,59 @@
-import type {RollupOptions} from 'rollup'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
-import terser from '@rollup/plugin-terser'
+import type { RollupOptions } from 'rollup';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-const production = process.env.NODE_ENV == 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
-export default <RollupOptions>{
+const rollupConfig: RollupOptions = {
   input: ['src/index.ts', 'src/server.ts'],
+
   output: {
     dir: 'build',
     format: 'cjs',
     sourcemap: true,
   },
+
   plugins: [
-    // this comment prevents prettier from formatting this array into a single line
-    resolve(),
+    /* Resolve node modules */
+    resolve({
+      browser: false,
+      preferBuiltins: true,
+    }),
+
+    /* Convert CommonJS modules to ES6 */
     commonjs(),
-    typescript({module: 'esnext'}),
-    production && terser(),
-  ].filter(Boolean),
+
+    /* Compile TypeScript */
+    typescript({
+      module: 'esnext',
+      target: 'es2019',
+      lib: ['es2019', 'DOM'],
+      sourceMap: true,
+    }),
+
+    /* Minify JavaScript in production */
+    isProduction && terser(),
+  ],
+
+  /* Specify external dependencies */
   external: ['vscode'],
-}
+
+  /* Watch for changes */
+  watch: {
+    include: 'src/**',
+    clearScreen: false,
+  },
+
+  /* Customize the Rollup cache */
+  cache: {
+    // Specify the maximum number of items that can be retained in the cache
+    maxEntries: 1000,
+
+    // Specify the maximum size of the cache in bytes
+    maxBytes: 100 * 1024 * 1024,
+  },
+};
+
+export default rollupConfig;
