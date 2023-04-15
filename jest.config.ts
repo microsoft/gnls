4a4ced41e2f3fb73509269725479ffd136dd4015
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-15 09:50:20 +0200

    Update jest.config.ts
    I added the following:
    
    testRunner option: This specifies the test runner to use. Jest supports several runners, including the default jest-jasmine and the newer jest-circus. jest-circus is recommended for larger projects because it provides better test isolation and performance.
    testMatch option: This specifies the file extensions to include in the tests. In this example, we're including all files with the .test.ts and .test.tsx extensions in the src directory.
    moduleNameMapper option: This allows you to map module names to paths, which can be useful for importing mocks and stubs. In this example, we're mapping the @ and ~ prefixes to the src and test directories, respectively.
    testPathIgnorePatterns option: This allows you to ignore files and directories when searching for tests. In this example, we're ignoring the node_modules directory.
    collectCoverage option: This enables Jest's code coverage feature, which measures how much of your code is being tested. The coverage information is output to the coverage directory.
    testEnvironment option: This specifies the environment in which the tests will be run. In this example, we're using the Node.js environment.
    verbose and maxWorkers options: These options allow you to customize the output of the test results. In this example, we're setting verbose to true to get more detailed output, and maxWorkers to 4 to parallelize the tests across multiple CPU cores.
    watchPathIgnorePatterns option: This allows you to ignore certain files and directories when watching for changes. In this example, we're ignoring the node_modules and .git directories.
    clearMocks option: This clears the mock calls between test runs, which can help prevent test leakage.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,46 @@
-import type {Config} from '@jest/types'
+import type { Config } from '@jest/types';
 
-export default <Config.InitialOptions>{
+const jestConfig: Config.InitialOptions = {
+  /* Specify the test runner */
+  testRunner: 'jest-circus/runner',
+
+  /* Specify the root directory of the project */
   roots: ['<rootDir>/src'],
-  transform: {'\\.tsx?$': 'ts-jest'},
-}
+
+  /* Specify the file extensions to include in the tests */
+  testMatch: ['**/*.test.(ts|tsx)'],
+
+  /* Transform TypeScript files using ts-jest */
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+
+  /* Map module names to paths, to avoid importing mocks and stubs */
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/test/$1',
+  },
+
+  /* Ignore files and directories when searching for tests */
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  /* Display coverage information */
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['lcov', 'text-summary'],
+
+  /* Specify the test environment */
+  testEnvironment: 'node',
+
+  /* Customize the output of test results */
+  verbose: true,
+  maxWorkers: 4,
+
+  /* Watch for changes */
+  watchPathIgnorePatterns: ['/node_modules/', '/.git/'],
+
+  /* Clear the cache between test runs */
+  clearMocks: true,
+};
+
+export default jestConfig;
